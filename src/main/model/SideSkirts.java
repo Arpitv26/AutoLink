a60dd87a83ce8a0f99ceeb0450044470c4eee396
arpit <arpitv@students.cs.ubc.ca>
2025-10-08 23:29:30 -0700

    implemented SideSkirts class

--- src/main/model/SideSkirts.java
+++ src/main/model/SideSkirts.java
@@ -9,22 +9,26 @@ public class SideSkirts extends Part {
     private final String material;
     private final String brand;
 
-    // REQUIRES: name/material/brand/finish non-null && non-empty; cost >= 0
+    // REQUIRES: name/material/brand non-null && non-empty; cost >= 0
     // MODIFIES: this
     // EFFECTS:  constructs side skirts with given specs
     public SideSkirts(String name, int cost, String material, String brand) {
         super(name, cost);
-        // stub
+        validateString(material, "material");
+        validateString(brand, "brand");
+
+        this.material = material;
+        this.brand = brand;
     }
 
     // EFFECTS: returns material
     public String getMaterial() {
-        return null;  // stub
+        return material;
     }
 
     // EFFECTS: returns brand
     public String getBrand() {
-        return null;  // stub
+        return brand;
     }
 
 }
